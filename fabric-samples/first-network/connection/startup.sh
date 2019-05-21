b45820c038d9ef62a2b5ec0c2ffa1e91d2a127e6
OlenaN <nesterenko.olena@gmail.com>
2019-05-21 16:21:44 +1000

    ...

--- fabric-samples/first-network/connection/startup.sh
+++ fabric-samples/first-network/connection/startup.sh
@@ -1,23 +1,21 @@
 ./certs.sh
-
+rm -r ~/.composer/cards/*
 
 rm *pem
 rm *_sk
 cp ../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/*_sk .
 cp ../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/A* .
 
+rm *.bna
 composer archive create -t dir -n ../productchain-network/
 cp ../productchain-network/productchain-network@0.0.1.bna .
 
 
 composer card create -p connection.json -u PeerAdmin -c *pem -k *_sk -r PeerAdmin -r ChannelAdmin
 
-composer card delete -c PeerAdmin@byfn-network
-composer card delete -c admin@productchain-network
-
 composer card import -f PeerAdmin@byfn-network.card --card PeerAdmin@byfn-network
 
-composer network install --card PeerAdmin@byfn-network --archiveFile ../productchain-network/productchain-network@0.0.1.bna
+composer network install --card PeerAdmin@byfn-network --archiveFile productchain-network@0.0.1.bna
 
 composer network start --networkName productchain-network --networkVersion 0.0.1 -A admin -S adminpw -c PeerAdmin@byfn-network
 
